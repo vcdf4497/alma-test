ad24f8ca6caa7de040ad741b1f1a34a9d19e29f6
midoledev <midoledev@gmail.com>
2026-01-31 15:10:41 +0100

    Update install_novnc.sh for dependency installation

--- install_novnc.sh
+++ install_novnc.sh
@@ -6,21 +6,17 @@ VNC_DISPLAY=:1
 VNC_PORT=5901
 
 echo "[+] Installation des dépendances"
-sudo pacman -Sy --needed --noconfirm \
+sudo pacman -Syu --needed --noconfirm \
   git \
   python \
-  python-websockify \
+  websockify \
   tigervnc
 
 echo "[+] Téléchargement de noVNC 1.6.0"
-if [ ! -d "noVNC" ]; then
-  git clone --branch v1.6.0 --depth 1 https://github.com/novnc/noVNC.git
-fi
+[ -d noVNC ] || git clone --branch v1.6.0 --depth 1 https://github.com/novnc/noVNC.git
 
 echo "[+] Lancement du serveur VNC ($VNC_DISPLAY)"
-if ! pgrep -f "Xvnc $VNC_DISPLAY" >/dev/null; then
-  vncserver $VNC_DISPLAY
-fi
+pgrep -f "Xvnc $VNC_DISPLAY" >/dev/null || vncserver $VNC_DISPLAY
 
 echo "[+] Lancement de noVNC (port $PORT)"
 cd noVNC
